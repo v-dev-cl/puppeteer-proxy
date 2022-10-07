9dfc84fa67f9dc47b872f9a3d320e22de6e49c26
Victor Ampuero <victor.amp.fern@gmail.com>
2022-10-07 16:56:02 -0300

    Fix

--- src/routines/getAllCookies.js
+++ src/routines/getAllCookies.js
@@ -6,5 +6,5 @@ import type {Page} from 'puppeteer';
  * @see https://stackoverflow.com/a/59604510/368691
  */
 export default (page: Page) => {
-  return page._client.send('Network.getAllCookies');
+  return page._client().send('Network.getAllCookies');
 };
